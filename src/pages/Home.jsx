e3a7bc3c7e216d68af8873afeee6f32c7c3e3c9f
Jobwafula <jobwafulabg@gmail.com>
2024-06-24 15:32:04 +0300

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Footer />
     </div>
   )
 }
